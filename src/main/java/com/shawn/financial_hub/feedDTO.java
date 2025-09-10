5ab121d5729238994d26910a2adfccc8dbac3e85
Shawn Lambert <shawnlambert004@gmail.com>
2025-09-10 15:48:17 +0100

    Feed DTO created, JSON body compatible

---
+++ src/main/java/com/shawn/financial_hub/feedDTO.java
@@ -0,0 +1,14 @@
+package com.shawn.financial_hub;
+
+public class feedDTO {
+    public String url;
+    public String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
