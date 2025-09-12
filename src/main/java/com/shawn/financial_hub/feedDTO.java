b7075cad519581c8d7ecb00a4c6050540bbdc409
Shawn Lambert <shawnlambert004@gmail.com>
2025-09-12 15:00:24 +0100

    Comments added

--- src/main/java/com/shawn/financial_hub/feedDTO.java
+++ src/main/java/com/shawn/financial_hub/feedDTO.java
@@ -3,6 +3,7 @@ package com.shawn.financial_hub;
 public class feedDTO {
     public String url;
     public String username;
+    public Long user_id;
 
     public String getUsername() {
         return username;
@@ -10,5 +11,9 @@ public class feedDTO {
 
     public String getUrl() {
         return url;
+    }
+
+    public Long getuser_ID() {
+        return user_id;
     }
 }
